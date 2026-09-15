cef279ee56f605f11b20a3b6dafdb48abb4ee326
Alejo <alejandroparral@gmail.com>
2026-09-15 11:15:01 -0500

    fix issues with static checks

--- src/main/java/co/com/japl/homeconnect/about/AboutActivity.kt
+++ src/main/java/co/com/japl/homeconnect/about/AboutActivity.kt
@@ -3,20 +3,15 @@ package co.com.japl.homeconnect.about
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.ui.platform.LocalContext
-import androidx.navigation.compose.rememberNavController
 import co.com.japl.homeconnect.about.ui.About
 import co.com.japl.ui.theme.MaterialThemeComposeUI
 
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
-            val navController = rememberNavController()
-            val context = LocalContext.current.applicationContext
             MaterialThemeComposeUI {
-                About("Module sin version","torressansebastian")
+                About("Module sin version", "torressansebastian")
             }
         }
     }
